6fdd38d79cc35ba38c3748ef3ec4c8be191f32f7
RiturajBH <riturajbp89@gmail.com>
2024-03-22 13:29:32 +0530

    light box added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // alert("hello!");
+
+// javascript for light-box modal
+const choosed_image = document.querySelectorAll("img");
+var image_source;
+choosed_image.forEach(img => {
+  img.addEventListener("click", () => {
+    image_source = img.getAttribute("src");
+    console.log(image_source);
+    document.getElementById("light-box").style.display = "block";
+    document.getElementById("light-box-img").setAttribute("src", image_source);
+  });
+});
+
+document.querySelector("#back").addEventListener("click", () => {
+    document.getElementById("light-box").style.display = "none";
+});
+
+
